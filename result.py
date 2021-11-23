e930d688a41f9696c02c77572d773a2759ab8b4d
armanbolatov <arman.bolatov@nu.edu.kz>
2021-11-23 14:42:33 +0600

    proper indentation for multiline strings

--- result.py
+++ result.py
@@ -12,74 +12,61 @@ def result_window(dna: str, selected_items: list) -> None:
     '''
     instances = seq.find_instances(dna, selected_items)
     positions = seq.find_positions(instances)
-    result53 =  "In the forward strand (5'-3') of the DNA sequence there are " + \
-                "following restriction sites.\n\n"
-    result35 =  "In the reversed strand (3'-5') of the DNA sequence there are " + \
-                "following restriction sites.\n\n"
     result_dna = seq.remove_instances(dna, instances)
-    if not bool(positions):
-        result53 = "There are no restriction sites in the forward strand of the given DNA."
-        result35 = "There are no restriction sites in the reversed strand of the given DNA."
+
+    result53 = ("In the forward strand (5'-3') of the DNA sequence there are "
+                "following restriction sites:\n\n") if bool(positions) else \
+               ("There are no restriction sites in the "
+                "forward strand of the given DNA.")
+    result35 = ("In the reversed strand (3'-5') of the DNA sequence there are "
+                "following restriction sites:\n\n") if bool(positions) else \
+               ("There are no restriction sites in the "
+                "reversed strand of the given DNA.")
+
     for key, values in positions.items():
         values = ", ".join(map(str, values))
-        result53 += key[0] + " named as " + \
-                    key[1] + " found at positions " + \
-                    values + ".\n\n"
-        result35 += seq.complement(key[0]) + " named as " + \
-                    key[1] + " found at positions " + \
-                    values + ".\n\n"
+        result53 += (f"{key[0]} named as {key[1]} found at "
+                     f"{'positions' if len(values) > 1 else 'position'} "
+                     f"{values}.\n\n")
+        result35 += (f"{seq.complement(key[0])} named as {key[1]} found at "
+                     f"{'positions' if len(values) > 1 else 'position'} "
+                     f"{values}.\n\n")
 
     if len(selected_items) == 1:
-        selected_restrictions = "The selected restriction site is " + \
-                                str(selected_items[0][0]) + " — " + \
-                                str(selected_items[0][1]) + "."
+        selected_rests = (f"The selected restriction site is "
+                          f"{selected_items[0][0]} — {selected_items[0][1]}.")
     elif len(selected_items) > 1:
-        selected_restrictions = "The selected restriction sites are " + \
-                                ", ".join([str(e[0]) + " — " + str(e[1]) \
-                                for e in selected_items]) + "."
+        rests = ", ".join([f"{e[0]} — {e[1]}" for e in selected_items])
+        selected_rests = f"The selected restriction sites are {rests}."
     else:
-        selected_restrictions = "You haven't yet selected any restriction sites. " + \
-                                "Please close this window, select the restrictions " + \
-                                "you are interested in, and run the program."
+        selected_rests = ("You haven't yet selected any restriction sites. "
+                          "Please close this window, select the restrictions "
+                          "you are interested in, and run the program.")
 
-    layout =  [[sg.Text("The forward strand of the DNA is:\n\n" + \
-                        "and the reversed one is",
-                        expand_y=True,
-                        expand_x=True,
-                        size=(35, None),
-                        key="desription"),
+    layout =  [[sg.Text(("The forward strand of the DNA is:\n\n"
+                         "and the reversed one is"),
+                        expand_y=True, expand_x=True,
+                        size=(35, None), key="desription"),
                 sg.Text(dna + '\n\n' + seq.complement(dna),
-                        expand_y=True,
-                        expand_x=True,
-                        size=(40, None),
-                        key="desription")],
-               [sg.Text(selected_restrictions,
-                       expand_y=True,
-                       expand_x=True,
-                       size=(80, None),
-                       key="desription")],
-               [sg.Multiline(result53,
-                             background_color='#64778D',
-                             text_color='white',
-                             key='result53',
-                             size=(37, 6),
-                             disabled=True),
-                sg.Multiline(result35,
-                             background_color='#64778D',
-                             text_color='white',
-                             key='result35',
-                             size=(37, 6),
-                             disabled=True)],
-                [sg.Text("The DNA sequences resulting after hiding the " + \
-                         "selected restrictions:")],
-                [sg.Multiline("(5\' - 3\'): " + result_dna + "\n\n(3\' - 5\'): " + \
-                              seq.complement(result_dna),
-                              background_color='#64778D',
-                              text_color='white',
-                              key='result_dna',
-                              size=(78, 4),
-                              disabled=True)]]
-    window = sg.Window("Resulting DNA", size=(None, None), modal=True).Layout(layout)
+                        expand_y=True, expand_x=True,
+                        size=(40, None), key="desription")],
+               [sg.Text(selected_rests,
+                        expand_y=True, expand_x=True,
+                        size=(80, None), key="desription")],
+               [sg.Multiline(result53, background_color='#64778D',
+                             text_color='white', key='result53',
+                             size=(37, 6), disabled=True),
+                sg.Multiline(result35, background_color='#64778D',
+                             text_color='white', key='result35',
+                             size=(37, 6), disabled=True)],
+                [sg.Text(("The DNA sequences resulting after hiding the "
+                          "selected restrictions:"))],
+                [sg.Multiline((f"(5\' - 3\'): {result_dna} \n\n(3\' - 5\'): "
+                               f"{seq.complement(result_dna)}"),
+                              background_color='#64778D', text_color='white',
+                              key='result_dna', size=(78, 4), disabled=True)]]
+
+    window = sg.Window("Resulting DNA", modal=True).Layout(layout)
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
